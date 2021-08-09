a4ddf77fa06ea7ef482e02adb8a91a022052e24f
xiaozou <1424113865@qq.com>
2021-08-09 17:45:41 +0800

    四种排序算法

---
+++ countSort.js
@@ -0,0 +1,24 @@
+let countSort = (arr) => {
+    let hashTable = {},
+        max = 0,
+        result = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (!(arr[i] in hashTable)) {
+            hashTable[arr[i]] = 1;
+        } else {
+            hashTable[arr[i]] += 1;
+        }
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+    }
+    for (let j = 0; j <= max; j++) {
+        if (j in hashTable) {
+            for (let i = 0; i < hashTable[j]; i++) {
+                result.push(j);
+            }
+        }
+    }
+    return result;
+};
+console.log(countSort.call(null, [3, 24, 6, 8, 4, 32, 87]));
---
+++ selectionSort.js
@@ -0,0 +1,24 @@
+let sort = (numbers) => {
+    for (let i = 0; i < numbers.length - 1; i++) {
+        let index = minIndex(numbers.slice(i)) + i;
+        if (index !== i) {
+            swap(numbers, index, i);
+        }
+    }
+    return numbers;
+};
+let swap = (array, i, j) => {
+    let temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+};
+let minIndex = (numbers) => {
+    let index = 0;
+    for (let i = 0; i < numbers.length; i++) {
+        if (numbers[i] < numbers[index]) {
+            index = i;
+        }
+    }
+    return index;
+};
+console.log(sort.call(null, [12, 4, 2, 6, 8, 19, 3]));
